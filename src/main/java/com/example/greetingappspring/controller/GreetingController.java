ace8750e14f73a774f570f392bf3668f24412954
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-14 17:27:07 +0530

    added:code of service layer

--- src/main/java/com/example/greetingappspring/controller/GreetingController.java
+++ src/main/java/com/example/greetingappspring/controller/GreetingController.java
@@ -1,21 +1,28 @@
 package com.example.greetingappspring.controller;
 
+import com.example.greetingappspring.controller.service.GreetingService;
 import com.example.greetingappspring.entity.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/greetingController")
 public class GreetingController {
+
+    @Autowired
+    GreetingService greetingService;
     private static final String TEMPLATE = "Hello,%s";
     private static final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = {"", "/", "/home"}, method = RequestMethod.GET)
     public User greeting(@RequestParam(value = "name", defaultValue = "Bridgelabz") String name) {
         return new User(counter.incrementAndGet(), String.format(TEMPLATE, name));
+    }
+
+    @GetMapping("/get")
+    public User sayHello() {
+        return greetingService.getMessage();
     }
 }
---
+++ src/main/java/com/example/greetingappspring/controller/service/GreetingService.java
@@ -0,0 +1,17 @@
+package com.example.greetingappspring.controller.service;
+
+import com.example.greetingappspring.entity.User;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@Service
+public class GreetingService {
+        private static final String TEMPLATE = "Hello ,%s !!";
+        private static final AtomicLong count = new AtomicLong();
+
+        public User getMessage() {
+            return new User(count.incrementAndGet(), String.format(TEMPLATE, "suraj"));
+        }
+}
+
